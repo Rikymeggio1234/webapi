067ec774897a4e217375959026104d3dbd36a541
Rikymeggio1234 <100524624+Rikymeggio1234@users.noreply.github.com>
2023-06-21 15:02:39 +0200

    template project

---
+++ WebApplication2/Controllers/QuestionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        [Route("helloworld/")]
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
